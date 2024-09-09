ab86ab841b178f16cbc00ce058f6bef42c06e1d4
gmlwlsdl <heejin094@naver.com>
2024-09-09 22:59:43 +0900

    chore: project setting
    - emotion 세팅
    - 전역 css 파일 초기화
    - 라우팅 세팅
    - 예시 페이지 home 추가

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import './index.css'
+
+import Home from './pages/home.tsx'
+
+const router = createBrowserRouter([{ path: '/home', element: <Home /> }])
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <RouterProvider router={router} />
+  </StrictMode>
 )
---
+++ src/pages/home.tsx
@@ -0,0 +1,7 @@
+import styled from '@emotion/styled/macro'
+
+function Home() {
+  return <h1>home</h1>
+}
+
+export default Home
