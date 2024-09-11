2e2d3778785c703e0dcd184b7aea90a2a024e1f1
boroboro01 <duke7201@naver.com>
2024-09-11 22:05:33 +0900

    feat: create background rectangle components

--- src/pages/Auth.tsx
+++ src/pages/Auth.tsx
@@ -1,5 +1,5 @@
 import styled from '@emotion/styled'
-import React from 'react'
+import React, { useState } from 'react'
 
 const Container = styled.div`
   display: flex;
@@ -19,7 +19,7 @@ const LoginForm = styled.input`
   background-color: #ededed;
   border: 2px solid #ededed;
   border-radius: 5px;
-  font-family: inter-light
+  font-family: inter-light;
   font-size: 20px;
   padding: 10px 15px;
   margin-bottom: 30px;
@@ -28,15 +28,64 @@ const LoginForm = styled.input`
 
   &:focus {
     outline: none;
-    border: 2px solid #ffa800; 
+    border: 2px solid #ffa800;
   }
 `
 
+const Button = styled.button<{ isPrimary: boolean }>`
+  background-color: ${(props) => (props.isPrimary ? '#ffa800' : '#ffffff')};
+  color: ${(props) => (props.isPrimary ? '#ffffff' : '#000000')};
+  font-family: ${(props) => (props.isPrimary ? 'inter-bold' : '#inter-medium')};
+  font-size: ${(props) => (props.isPrimary ? '30px' : '26px')};
+  border: none;
+  border-radius: 5px;
+  padding: 5px 10px;
+  margin-bottom: 15px;
+  cursor: pointer;
+  width: 100%;
+`
+
+const Image = styled.img<{
+  top?: string
+  bottom?: string
+  left?: string
+  right?: string
+}>`
+  position: absolute;
+  top: ${({ top }) => top || 'auto'};
+  bottom: ${({ bottom }) => bottom || 'auto'};
+  left: ${({ left }) => left || 'auto'};
+  right: ${({ right }) => right || 'auto'};
+`
+
 const Auth = () => {
+  const [isPrimary, setIsPrimary] = useState(true)
+
   return (
     <Container>
       <div>
+        <Image
+          src="../src/assets/images/left-top.svg"
+          top={'0'}
+          left={'0'}
+        ></Image>
+        <Image
+          src="../src/assets/images/right-top.svg"
+          top={'0'}
+          right={'0'}
+        ></Image>
+        <Image
+          src="../src/assets/images/left-bottom.svg"
+          bottom={'0'}
+          left={'0'}
+        ></Image>
+        <Image
+          src="../src/assets/images/right-bottom.svg"
+          bottom={'0'}
+          right={'0'}
+        ></Image>
         <div>
+          <img src="../src/assets/images/logo.svg"></img>
           <form>
             <div>
               <FormLabel>닉네임</FormLabel>
@@ -46,6 +95,8 @@ const Auth = () => {
               <FormLabel>비밀번호</FormLabel>
               <LoginForm></LoginForm>
             </div>
+            <Button isPrimary={isPrimary}>로그인</Button>
+            <Button isPrimary={!isPrimary}>회원가입</Button>
           </form>
         </div>
       </div>
