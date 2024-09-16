ace6918bb71781cc85345f52ba7ab83a690df5aa
boroboro01 <duke7201@naver.com>
2024-09-16 17:03:11 +0900

    refactor: seperated popup into its own component

---
+++ src/components/Popup.tsx
@@ -0,0 +1,85 @@
+import styled from '@emotion/styled'
+
+interface Props {
+  text: string
+  onClick?: () => void
+}
+
+const PopupContainer = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5); /* Semi-transparent background */
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  z-index: 1000;
+  backdrop-filter: blur(5px);
+`
+
+const PopupContent = styled.div`
+  background-color: white;
+  padding: 20px;
+  border-radius: 10px;
+  box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.2);
+  text-align: center;
+`
+
+const Title = styled.p`
+  font-family: Pretendard;
+  font-style: semibold;
+  font-size: 30px;
+  font-weight: 600;
+  text-align: center;
+  white-space: pre-line;
+  line-height: 150%;
+  letter-spacing: -1.1%;
+`
+
+const RoundedButton = styled.button<{
+  onClick: () => void
+  isPrimary?: boolean
+}>`
+  display: inline-block;
+  background-color: ${(props) => (props.isPrimary ? '#ffa800' : '#ffffff')};
+  color: ${(props) => (props.isPrimary ? '#ffffff' : '#ffa800')};
+  font-family: Pretendard;
+  font-style: semibold;
+  font-size: 30px;
+  font-weight: 700;
+  border: 4px solid #ffa800;
+  border-radius: 24px;
+  cursor: pointer;
+  width: 226px;
+  height: 82px;
+`
+
+const ButtonContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  gap: 30px;
+  margin-top: 60px;
+  margin-left: 21px;
+  margin-right: 21px;
+  margin-bottom: 26px;
+`
+
+const Popup = ({ text, onClick }: Props) => {
+  return (
+    <PopupContainer>
+      <PopupContent>
+        <Title>{text}</Title>
+        <ButtonContainer>
+          <RoundedButton onClick={onClick || (() => {})}>아니요</RoundedButton>
+          <RoundedButton onClick={onClick || (() => {})} isPrimary={true}>
+            네
+          </RoundedButton>
+        </ButtonContainer>
+      </PopupContent>
+    </PopupContainer>
+  )
+}
+
+export default Popup
--- src/components/SearchBox.tsx
+++ src/components/SearchBox.tsx
@@ -3,6 +3,7 @@ import SearchBar from './SearchBar'
 import TextButton from './TextButton'
 import { TiPlus } from 'react-icons/ti'
 import { useState } from 'react'
+import Popup from './Popup'
 
 const Container = styled.div`
   display: flex;
@@ -64,67 +65,6 @@ const Icon = styled(TiPlus)`
 
 const items = Array.from({ length: 30 }, (_, i) => `Name ${i + 1}`)
 
-const PopupContainer = styled.div`
-  position: fixed;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  background-color: rgba(0, 0, 0, 0.5); /* Semi-transparent background */
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  z-index: 1000;
-  backdrop-filter: blur(5px);
-`
-
-const Popup = styled.div`
-  background-color: white;
-  padding: 20px;
-  border-radius: 10px;
-  box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.2);
-  text-align: center;
-`
-
-const Title = styled.p`
-  font-family: Pretendard;
-  font-style: semibold;
-  font-size: 30px;
-  font-weight: 600;
-  text-align: center;
-  white-space: pre-line;
-  line-height: 150%;
-  letter-spacing: -1.1%;
-`
-
-const RoundedButton = styled.button<{
-  onClick: () => void
-  isPrimary?: boolean
-}>`
-  display: inline-block;
-  background-color: ${(props) => (props.isPrimary ? '#ffa800' : '#ffffff')};
-  color: ${(props) => (props.isPrimary ? '#ffffff' : '#ffa800')};
-  font-family: Pretendard;
-  font-style: semibold;
-  font-size: 30px;
-  font-weight: 700;
-  border: 4px solid #ffa800;
-  border-radius: 24px;
-  cursor: pointer;
-  width: 226px;
-  height: 82px;
-`
-
-const ButtonContainer = styled.div`
-  display: flex;
-  justify-content: center;
-  gap: 30px;
-  margin-top: 60px;
-  margin-left: 21px;
-  margin-right: 21px;
-  margin-bottom: 26px;
-`
-
 const SearchBox = () => {
   const [isPopupOpen, setIsPopupOpen] = useState(false)
 
@@ -153,17 +93,10 @@ const SearchBox = () => {
         onClick={openPopup}
       ></TextButton>
       {isPopupOpen && (
-        <PopupContainer>
-          <Popup>
-            <Title>가족 구성원 모집을{'\n'} 완료 하시겠습니까?</Title>
-            <ButtonContainer>
-              <RoundedButton onClick={closePopup}>아니요</RoundedButton>
-              <RoundedButton onClick={closePopup} isPrimary={true}>
-                네
-              </RoundedButton>
-            </ButtonContainer>
-          </Popup>
-        </PopupContainer>
+        <Popup
+          text={'가족 구성원 모집을\n 완료 하시겠습니까?'}
+          onClick={closePopup}
+        ></Popup>
       )}
     </Container>
   )
