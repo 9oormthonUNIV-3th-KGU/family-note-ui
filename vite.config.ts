fa3050d059785128eafb98be91a8143104796143
gmlwlsdl <heejin094@naver.com>
2024-09-24 16:40:15 +0900

    fix: Update vite.config.ts
    secure true > false

--- vite.config.ts
+++ vite.config.ts
@@ -9,12 +9,12 @@ export default defineConfig({
       '/family': {
         target: '//211.188.49.236:5252/api/v1',
         changeOrigin: true,
-        secure: true,
+        secure: false,
       },
       '/users': {
         target: '//211.188.49.236:5252/api/v1',
         changeOrigin: true,
-        secure: true,
+        secure: false,
       },
     },
     hmr: {
